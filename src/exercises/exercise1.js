90307d6e17eafe93d1acf988cfa9e96c61bef70f
Thomas Teague <thomas.d.teague1@gmail.com>
2022-01-24 11:53:08 -0500

    Improve navigation for business categories
    First, add a NARROWER_THAN relationship to the graph to introduce
    a hierarchy of business categories. Base the NARROWER_THAN relationship
    on the Overlap Similarity algorithm in the Graph Data Science Library.
    Then, modify the query that populates the Business Search drop down menu
    to include only the top-level categories.

--- src/exercises/exercise1.js
+++ src/exercises/exercise1.js
@@ -1,12 +1,7 @@
 
-export const FETCH_CATEGORIES_QUERY =
-`
+export const FETCH_CATEGORIES_QUERY = `
 MATCH (c:Category)
-// Edit this part to filter only top level categories 
-// Top level categories don’t have an outgoing NARROWER_THAN relationship.
-
+WHERE NOT EXISTS ((c)-[:NARROWER_THAN]->())
 WITH c ORDER BY c.name LIMIT 500
 RETURN COLLECT(c.name) AS categories
 `;
-
-// The solution for this exercise is available in  src/solutions/exercise1.js
