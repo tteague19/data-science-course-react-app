d241efbad4ba85f082204d938987b67b3993c353
Thomas Teague <thomas.d.teague1@gmail.com>
2022-01-24 12:00:19 -0500

    Improve the business search results
    Order the business search results to include the most relevant
    businesses for the "logged in" user. Use the Pearson similarity metric
    and create a SIMILAR relationship between the user and other reviewers.
    Then, order the businesses by the average score that users with the
    SIMILAR relationship to the user gave to the business.

--- src/exercises/exercise2.js
+++ src/exercises/exercise2.js
@@ -1,21 +1,12 @@
-/*
-TODO: rank businesses that are more relevant
- */
-export const FETCH_BUSINESSES_QUERY = 
-`
-CALL db.index.fulltext.queryNodes("BusinessNameIndex", $searchText + "~0.75") YIELD node, score
+export const FETCH_BUSINESSES_QUERY = `
+CALL db.index.fulltext.queryNodes("BusinessNameIndex", $searchText + "~0.75")
+YIELD node, score
 WITH node AS b ORDER BY score DESC LIMIT 200
 MATCH (b)-[:IN_CATEGORY]->(c:Category {name: $category})
 OPTIONAL MATCH (b)-[:HAS_PHOTO]->(p:Photo)
 WITH b, COLLECT(p)[0] AS p
-// Match businesses who have been reviewed by similar users
-// Hint: Use $userId parameter
-
-// Return the businesses that were reviewed by the most similar users
-// Hint: Use the average of similarity score stored in the SIMILAR relationships 
-
-WITH b,p  ORDER BY EXISTS((b)-[:HAS_PHOTO]->()) DESC LIMIT 100
-RETURN COLLECT(b {.*, photo: p.id}) AS businesses
+MATCH (me:User {id: $userId})
+OPTIONAL MATCH (me)-[similarity:SIMILAR]-(other)-[:WROTE]-(:Review)-[:REVIEWS]->(b)
+WITH DISTINCT b, p, avg(coalesce(similarity.score, 0)) AS avgScore ORDER BY avgScore DESC LIMIT 100
+RETURN COLLECT(b {.*, photo: p.id, score: avgScore}) AS businesses
 `;
-
-// The solution for this exercise is available in  src/solutions/exercise2.js
